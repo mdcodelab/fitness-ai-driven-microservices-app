64f4a2e13c19d6372e93dbb65ee50959ac463058
Mihaela Diaconu <contact.mihaela.office@gmail.com>
2026-02-17 15:23:35 +0200

    schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiGatewayModule } from './api-gateway/api-gateway.module';
+import { UsersModule } from './users/users.module';
+import { ActivitiesModule } from './activities/activities.module';
+import { AiModule } from './ai/ai.module';
 
 @Module({
-  imports: [],
+  imports: [ApiGatewayModule, UsersModule, ActivitiesModule, AiModule],
   controllers: [AppController],
   providers: [AppService],
 })
