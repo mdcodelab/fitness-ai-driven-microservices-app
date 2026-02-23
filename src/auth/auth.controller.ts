5144420c05fa6e81a301eff225536569404e105c
Mihaela Diaconu <contact.mihaela.office@gmail.com>
2026-02-23 23:11:50 +0200

    auth controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Res} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from '../../libs/dto/auth.dto';
+import type { Response as ExpressResponse } from 'express';
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+constructor(private readonly authService: AuthService) {}
+
+    @Post('register')
+    register(@Body() dto: AuthDto){
+        return this.authService.register(dto)
+    }
+
+    @Post('login')
+    login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: ExpressResponse){
+        return this.authService.login(dto, res)
+    }
+
+    @Post("logout")
+    logout(@Res({ passthrough: true }) res: ExpressResponse){
+        return this.authService.logout(res);
+    }
+}
